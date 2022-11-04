2ebb51e7f13488744aeba4623a4752c3689d8135
Min <biffboffkev@gmail.com>
2022-11-04 17:03:50 -0400

    need to fix post submission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,11 @@ class PostsController < ApplicationController
         render json: Post.all
     end
 
+    def create
+        new_post = Post.create!(user:current_user, book_id:params[:bookId], title:params[:title], content:params[:content])
+        render json: new_post, status: :ok
+    end
+
     def show
         render json: Post.find_by!(id:params[:id])
     end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,6 +2,9 @@ class PostSerializer < ActiveModel::Serializer
     attributes :id, :user, :book, :title, :content, :created_at, :number_of_comments
 
     belongs_to :book
+    
+    # this code might make the call stack too big
+    has_many :comments
 
     def user
       self.object.user.username
