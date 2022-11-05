58b20955aade0ed243737793391807d52191dfd8
Min <biffboffkev@gmail.com>
2022-11-05 00:21:01 -0400

    part 1 of book search

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,8 @@
 class CommentsController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        @new_comment = Comment.create!(user:current_user, post_id:params[:postId], content:params[:content])
+        render json: @new_comment, status: :ok
+    end
 end
