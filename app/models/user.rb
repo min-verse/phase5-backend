b033f05447e08ddd66079ab38fcf58759797fd45
Min <biffboffkev@gmail.com>
2022-11-07 17:04:50 -0500

    tweaked profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # has_one_attached :profile_picture
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable, :validatable,
